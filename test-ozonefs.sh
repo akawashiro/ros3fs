449d32e0552e00ebe6a09abdc9d7f832684ca7fe
Akira Kawata <akirakawata1@gmail.com>
2023-03-16 22:11:50 +0900

    Add tests

--- test-ozonefs.sh
+++ test-ozonefs.sh
@@ -1,6 +1,7 @@
 #! /bin/bash -u
 
 cd $(git rev-parse --show-toplevel)/build
+exit_code=0
 
 echo "=========== ls test =========="
 OZONEFS_TMPFILE=$(mktemp)
@@ -11,7 +12,7 @@ ls ozonefs_mountpoint_answer >& ${NORMALFS_TMPFILE}
 if [[ $(diff ${OZONEFS_TMPFILE} ${NORMALFS_TMPFILE}) ]]; then
     diff ${OZONEFS_TMPFILE} ${NORMALFS_TMPFILE}
     echo "test failed"
-    exit 1
+    exit_code=1
 fi
 
 echo "=========== ls -F test =========="
@@ -23,5 +24,31 @@ ls -F ozonefs_mountpoint_answer >& ${NORMALFS_TMPFILE}
 if [[ $(diff ${OZONEFS_TMPFILE} ${NORMALFS_TMPFILE}) ]]; then
     diff ${OZONEFS_TMPFILE} ${NORMALFS_TMPFILE}
     echo "test failed"
-    exit 1
+    exit_code=1
 fi
+
+echo "=========== cat test =========="
+OZONEFS_TMPFILE=$(mktemp)
+NORMALFS_TMPFILE=$(mktemp)
+cat ozonefs_mountpoint/testfile_a >& ${OZONEFS_TMPFILE}
+cat ozonefs_mountpoint_answer/testfile_a >& ${NORMALFS_TMPFILE}
+
+if [[ $(diff ${OZONEFS_TMPFILE} ${NORMALFS_TMPFILE}) ]]; then
+    diff ${OZONEFS_TMPFILE} ${NORMALFS_TMPFILE}
+    echo "test failed"
+    exit_code=1
+fi
+
+echo "=========== grep -r test =========="
+OZONEFS_TMPFILE=$(mktemp)
+NORMALFS_TMPFILE=$(mktemp)
+grep -r bb ozonefs_mountpoint >& ${OZONEFS_TMPFILE}
+grep -r bb ozonefs_mountpoint_answer >& ${NORMALFS_TMPFILE}
+
+if [[ $(diff ${OZONEFS_TMPFILE} ${NORMALFS_TMPFILE}) ]]; then
+    diff ${OZONEFS_TMPFILE} ${NORMALFS_TMPFILE}
+    echo "test failed"
+    exit_code=1
+fi
+
+exit ${exit_code}
