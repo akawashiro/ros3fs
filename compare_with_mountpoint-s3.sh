4b839b38b7f07f29e0c006493250327c8db90eaa
Akira Kawata <akirakawata1@gmail.com>
2023-10-15 23:50:49 +0900

    Compare with s3fs

--- compare_with_mountpoint-s3.sh
+++ compare_with_mountpoint-s3.sh
@@ -13,14 +13,28 @@ if [[ ! -d build/mountpoint-s3 ]]; then
     popd
 fi
 
+# Get OZONE_OM_IP
+OZONE_OM_IP=$(sudo docker inspect --format='{{.NetworkSettings.Networks.bridge.Gateway}}' ozone-instance)
+
+# Mount ros3fs
+umount ./build/ros3fs_mountpoint || true
+./build/ros3fs ./build/ros3fs_mountpoint --endpoint=http://${OZONE_OM_IP}:9878 \
+    --bucket_name=bucket1/ --cache_dir=./build/ros3fs_cache_dir --update_seconds=100 --clear_cache &
+
+# Mount s3fs-fuse
+if [[ ! -d build/s3fs-fuse_mountpoint ]]; then
+    mkdir -p build/s3fs-fuse_mountpoint
+fi
+umount ./build/s3fs-fuse_mountpoint || true
+s3fs bucket1 build/s3fs-fuse_mountpoint  -o url=http://${OZONE_OM_IP}:9878 -o use_path_request_style
+
+# Mount mountpoint-s3
 if [[ ! -d build/mountpoint-s3_mountpoint ]]; then
     mkdir -p build/mountpoint-s3_mountpoint
 fi
 umount ./build/mountpoint-s3_mountpoint || true
-OZONE_OM_IP=$(sudo docker inspect --format='{{.NetworkSettings.Networks.bridge.Gateway}}' ozone-instance)
 ./build/mountpoint-s3/target/release/mount-s3  --endpoint-url=http://${OZONE_OM_IP}:9878 bucket1 ./build/mountpoint-s3_mountpoint
 
-hyperfine --ignore-failure "grep -Rnw ./build/ros3fs_mountpoint -e '123'"
-hyperfine --ignore-failure "grep -Rnw ./build/mountpoint-s3_mountpoint -e '123'"
-
-umount ./build/mountpoint-s3_mountpoint
+hyperfine --ignore-failure --warmup 3 "grep -Rnw ./build/ros3fs_mountpoint -e '123'" 2>/dev/null
+hyperfine --ignore-failure --warmup 3 "grep -Rnw ./build/s3fs-fuse_mountpoint -e '123'" 2>/dev/null
+hyperfine --ignore-failure --warmup 3 "grep -Rnw ./build/mountpoint-s3_mountpoint -e '123'" 2>/dev/null
